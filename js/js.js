0f2d7872f86e3c715d8e653f0fb31e045742502f
Mike <mquon@outlook.com>
2018-01-13 23:09:14 -0800

    new updates

--- js/js.js
+++ js/js.js
@@ -1,8 +1,12 @@
-<<<<<<< HEAD
-function(){
-    
-=======
-function(){
-    
->>>>>>> first commit
+var playerChoice = 0;
+var compChoice = Math.random();
+
+if(compChoice < 0.34){
+    compChoice="Rock";
+}
+else if(compChoice <= 0.67){
+    compChoice="Paper";
+
+}else{
+    compChoice="Scissor";
 }
